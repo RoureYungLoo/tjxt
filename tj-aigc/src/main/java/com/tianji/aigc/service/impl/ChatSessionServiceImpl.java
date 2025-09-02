edb6eb73334bdf5f8015c01e0bd196505ba28ebe
luruoyang <ruoyang8715@163.com>
2025-09-03 00:34:30 +0800

    feat: 新增: 新建会话
    新建会话

---
+++ tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatSessionServiceImpl.java
@@ -0,0 +1,60 @@
+package com.tianji.aigc.service.impl;
+
+import cn.hutool.core.util.IdUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tianji.aigc.entity.ChatSession;
+import com.tianji.aigc.mapper.ChatSessionMapper;
+import com.tianji.aigc.properties.SessionProperties;
+import com.tianji.aigc.service.IChatSessionService;
+import com.tianji.aigc.vo.SessionVO;
+import com.tianji.common.utils.UserContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * <p>
+ * 对话session 服务实现类
+ * </p>
+ *
+ * @author mr.wu
+ * @since 2025-08-17
+ */
+@Service
+public class ChatSessionServiceImpl extends ServiceImpl<ChatSessionMapper, ChatSession> implements IChatSessionService {
+
+    @Autowired
+    private SessionProperties sessionProperties;
+
+    /**
+     * 新建会话
+     *
+     * @param num
+     * @return
+     */
+    @Override
+    public SessionVO createSession(Integer num) {
+        //1.生成会话ID
+        String sessionId = IdUtil.fastSimpleUUID();
+
+        //2.保存会话历史
+        ChatSession chatSession = ChatSession.builder().sessionId(sessionId)
+                .userId(UserContext.getUser()).build();
+        this.save(chatSession);
+
+        //3.随机取出3个热门问题
+        List<SessionVO.Example> examples = sessionProperties.getExamples();
+        Collections.shuffle(examples);
+
+        //4.封装响应结果
+        SessionVO sessionVO = SessionVO.builder()
+                .sessionId(sessionId)
+                .describe(sessionProperties.getDescribe())
+                .examples(examples.stream().limit(num).toList())
+                .build();
+
+        return sessionVO;
+    }
+}
